f2bc461b6635122bd7841b7d48e957e9ae3bf381
unknown <diah.afia@ui.ac.id>
2022-10-30 08:12:05 +0700

    pbp

--- login/urls.py
+++ login/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from .views import signup
+from .views import signup, login_page, landing_page
 from django.views.decorators.csrf import csrf_exempt
 
-
+app_name = 'login'
 
 urlpatterns = [
-    path('', signup, name='signup'),
+    path('', landing_page, name='landing_page'),
+    path('login/', login_page, name='login_page'),
+    path('signup/', signup, name='signup'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render, redirect
 from django.http.response import JsonResponse
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 import json
-from .forms import RegistrationForm
+from .forms import RegistrationForm, UserAuthenticationForm
+
+#@login_required(login_url='/login/login_page/')
+def landing_page(request):
+    return render(request, 'landing.html')
+
+def login_page(request):
+	context = {}
+	user = request.user
+	if user.is_authenticated:
+		return redirect('daftar_project:index_daftar_project')
+
+	if request.POST:
+		form = UserAuthenticationForm(request.POST)
+		if form.is_valid():
+			email = request.POST['email']
+			password = request.POST['password']
+			user = authenticate(email=email, password=password)
+			if user:
+				login(request, user)
+				return redirect('daftar_project:index_daftar_project')
+				
+	else:
+		form = UserAuthenticationForm()
+	context['login_form'] = form
+
+	return render(request, "login.html", context)
 
 def signup(request):
 	user = request.user
