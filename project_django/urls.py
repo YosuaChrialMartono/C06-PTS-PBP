5b23633b681a9ecf311725083a60e66cd766d077
Made Swastika Nata Negara <swastika.nata1804@gmail.com>
2022-11-01 20:38:53 +0700

    swas: forum app finished

--- project_django/urls.py
+++ project_django/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
     path('article/', include('article.urls')),
+    path('forum/', include('forum.urls')),
 ]
