841846add02f2f74784cf0dc97ecdd5b8edf47fd
kkoep <94103377+kkoep@users.noreply.github.com>
2022-12-11 21:41:55 +0700

    d

--- authentication/views.py
+++ authentication/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
 @csrf_exempt
 def login(request):
     email = request.POST['email']
@@ -12,7 +13,7 @@ def login(request):
     user = authenticate(email=email, password=password)
     if user is not None:
         if user.is_active:
-            login(request, user)
+            auth_login(request, user)
             # Redirect to a success page.
             return JsonResponse({
               "status": True,
