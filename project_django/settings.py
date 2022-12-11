9b361d0dacd264f28b567ab52d9fe082b33811b5
kkoep <94103377+kkoep@users.noreply.github.com>
2022-12-11 20:27:42 +0700

    dsf

--- project_django/settings.py
+++ project_django/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [f'https://{APP_NAME}.up.railway.app']
+CSRF_TRUSTED_ORIGINS = [f'https://pts-c06-pbp.up.railway.app']
 
 # Application definition
 
